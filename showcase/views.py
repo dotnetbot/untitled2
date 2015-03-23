fa70c1a4770dd9cbedb54923f0ad7f8f42d21621
dotnetbot <dotnetbot@gmail.com>
2015-03-23 10:08:25 +0500

    added django-rest-framework

--- showcase/views.py
+++ showcase/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from showcase.serializers import UserSerializer, GroupSerializer
 
 
 def index(request):
@@ -8,3 +11,13 @@ def index(request):
     context = RequestContext(request)
 
     return HttpResponse(template.render(context))
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
